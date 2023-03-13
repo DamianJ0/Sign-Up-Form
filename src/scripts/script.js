8de699f8cd60ee51792dee95bbc6cd739f6d5772
Damian Jaworowski <damianjaworowski0@gmail.com>
2023-03-13 22:48:53 +0100

    commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,7 +6,7 @@ const btn = document.querySelector('button')
 const passwordValidation1 = document.querySelector('.password-validation1')
 const passwordValidation2 = document.querySelector('.password-validation2')
 const nicknameValidation = document.querySelector('.nickname-validation')
-const emailValidation = document.querySelector('.email.validation')
+const emailValidation = document.querySelector('.email-validation')
 const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
 const letters = /[a-z]/i;
 const numbers = /[0-9]/;
@@ -22,19 +22,21 @@ const validation = () => {
         messagePasswd='Hasło musi zawierać co najmniej 10 znaków w tym jedna litere, jedna cyfre i jeden znak specjalny!';
     }
     passwordValidation1.textContent = messagePasswd;
-    if(checkPass !== pass){
-        messagePasswd2='Podane hasła są różne'
+    if(checkPass === pass){
+        messagePasswd2=''
     }else{
-        messagePasswd2=""
+        messagePasswd2='Podane hasła są różne'
     }
     passwordValidation2.textContent = messagePasswd2;
 
-    if(nick.value.match(special)){
+    if(!regex.test(email)){
+        
+    } else {
         messageNickname = 'Nickname nie może zawierać znaków specjalnych'
     }
     nicknameValidation.textContent = messageNickname;
     
-    if(email.match(regex)){
+    if(regex.test(email)){
     }else {
         messageEmail = 'Podany email jest błędny'
     }
