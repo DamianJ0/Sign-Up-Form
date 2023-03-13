4920e6fe062b922f04d6790ef31608c66bcfe4eb
Damian Jaworowski <damianjaworowski0@gmail.com>
2023-03-13 21:52:18 +0100

    commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,19 +1,48 @@
 const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
+const nick = document.querySelector('#nickname')
+const checkPass = document.querySelector('#check-password')
+const email = document.querySelector('#email')
 const btn = document.querySelector('button')
-const passwordValidation = document.querySelector('.password-validation')
+const passwordValidation1 = document.querySelector('.password-validation1')
+const passwordValidation2 = document.querySelector('.password-validation2')
+const nicknameValidation = document.querySelector('.nickname-validation')
+const emailValidation = document.querySelector('.email.validation')
+const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
 const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
-let message = "";
+let messagePasswd = "";
+let messagePasswd2 = "";
+let messageNickname= "";
+let messageEmail = "";
 
-const checkPasswd= () => {
-     if(pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)){
-        message='Hasło musi zawierać co najmniej 10 znaków w tym jedna litere, jedna cyfre i jeden znak specjalny!';
+const validation = () => {
+     if(pass.value.length < minValue){
+        messagePasswd='Hasło musi zawierać co najmniej 10 znaków w tym jedna litere, jedna cyfre i jeden znak specjalny!';
     }
-    passwordValidation.textContent = message;
+    passwordValidation1.textContent = messagePasswd;
+    if(checkPass !== pass){
+        messagePasswd2='Podane hasła są różne'
+    }else{
+        messagePasswd2=""
+    }
+    passwordValidation2.textContent = messagePasswd2;
+
+    if(nick.value.match(special)){
+        messageNickname = 'Nickname nie może zawierać znaków specjalnych'
+    }
+    nicknameValidation.textContent = messageNickname;
+    
+    if(email.match(regex)){
+    }else {
+        messageEmail = 'Podany email jest błędny'
+    }
+    emailValidation.textContent = messageEmail
 }
-btn.addEventListener('click', checkPasswd)
+
+btn.addEventListener('click', validation)
+
+
 
 
